cfc6ce8eaed0ffc5b3c47684f5734028a1e9843a
lehoanphuc <hoanphucle2001@gmail.com>
2022-04-01 11:49:59 +0700

    Add project files.

---
+++ demoWeb/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demoWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Book()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Menu() 
+        {
+            ViewBag.Message = "You menu page";
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your Form Login";
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            ViewBag.Message = "Your Form Login";
+            return View();
+        }
+
+        public ActionResult TinhThanh()
+        {
+            ViewBag.Message = "Your Form Login";
+            return View();
+        }
+    }
+}
